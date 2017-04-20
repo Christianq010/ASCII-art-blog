8c621db29557593fc46b36589058da06bc2fa781
Christiaan Quyn <christianq010@gmail.com>
2017-04-20 23:38:19 +0530

    feat: Add title and art to google.appengine Database and query data
    Run a query to retreieve data and display it back onto the page.

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ import os
 import webapp2
 import jinja2
 
+from google.appengine.ext import db
+
 # Our templates to be stored in a templates folder
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir),
@@ -36,19 +38,32 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render("front.html", title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * FROM Art "
+                           "ORDER BY created DESC ")
+
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render_front()
 
     def post(self):
-        title = self.request.get("title!")
+        title = self.request.get("title")
         art = self.request.get("art")
 
         if title and art:
-            self.write("thanks")
+            a = Art(title=title, art=art)
+            a.put()
+
+            self.redirect("/")
         else:
             error = "we need both a title and some artwork!"
             self.render_front(title, art, error)
