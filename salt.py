193377a5d94a2b724f2be46133552f2b28e4cdf8
Christiaan Quyn <christianq010@gmail.com>
2017-04-22 11:13:40 +0530

    refactor: Validating our salt use for hashing

--- salt.py
+++ salt.py
@@ -10,17 +10,21 @@ def make_salt():
     return ''.join(random.choice(string.letters) for x in xrange(5))
 
 
-# print make_salt()
+# Implement the function valid_pw() that returns True if a user's password
+# matches its hash. You will need to modify make_pw_hash.
 
-# implement the function make_pw_hash(name, pw) that returns a hashed password
-# of the format:
-# HASH(name + pw + salt),salt
-# use sha256
-
-def make_pw_hash(name, pw):
-    salt = make_salt()
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (h, salt)
 
-print make_pw_hash('spez', 'hunterz')
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name,pw, h)
+
+h = make_pw_hash('spez', 'hunter2')
+print h
+print valid_pw('spez', 'hunter2', h)
 
